79c1adb88a5e999b8c2e5e64155c0e407bf6fddc
Artem Vovsia <avovsya@gmail.com>
2017-01-31 22:53:12 +0000

    Most influential characters

--- report.js
+++ report.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var Graph = require('ngraph.graph');
+var pagerank = require('ngraph.pagerank');
 
 var cache;
 
@@ -30,6 +32,55 @@ function print10MostPopularCharactersFromCache() {
   console.log('Most popular characters:');
   var characters = getMostPopularCharacters(cache, 10);
   _.each(characters, (c) => console.log(`${c.name}: ${c.comics.available}`));
+  console.log('===');
+}
+
+function buildComicsHashmap(characters) {
+  var comics = {};
+
+  _.each(characters, (character) => {
+    _.each(character.comics.items, (c) => {
+      if(!comics[c.name]) { comics[c.name] = []; }
+      comics[c.name].push(character.name);
+    });
+  });
+
+  return comics;
+}
+
+function buildGraph(comicsHash) {
+  var i;
+  var graph = Graph();
+
+  _.each(comicsHash, (comics) => {
+    for (i = 0; i < comics.length - 1; i += 1) {
+      graph.addLink(comics[i], comics[i+1]);
+      graph.addLink(comics[i+1], comics[i]);
+    }
+  });
+
+  return graph;
+}
+
+// For each comics gather bucket of characters from that comics
+// Connect characters from the same comics with the vertex
+function print10MostInfluentialCharactersFromCache() {
+  var comicsHash = buildComicsHashmap(cache);
+  var graph = buildGraph(comicsHash);
+  var rank = pagerank(graph);
+
+  var top10 = _(rank)
+      .map((rank, name) => ({ name, rank }))
+      .orderBy((c) => c.rank, 'desc')
+      .take(10)
+      .value();
+
+  console.log('');
+  console.log('Most influential characters:');
+  console.log(top10);
+  console.log('===');
+  // console.log(JSON.stringify(comicsHash, ' ', 2));
 }
 
 print10MostPopularCharactersFromCache();
+print10MostInfluentialCharactersFromCache();
